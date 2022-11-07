a22ffbe4bc19bb2146743aa0a5537eadd3317155
Soukaina Ouchen <souchen@e2r8p1.1337.ma>
2022-11-07 12:27:41 +0100

    add a map

---
+++ raycast.js
@@ -0,0 +1,40 @@
+//create a class of 2Dmap
+const MAP_NUM_ROWS = 11;
+const MAP_NUM_COLS = 15;
+const TAILLE_SIZE = 32;
+const WINDOZ_WIDTH = MAP_NUM_COLS * TAILLE_SIZE;
+const WINDOZ_WEIGHT = MAP_NUM_ROWS * TAILLE_SIZE;
+class Map {
+    constructor() {
+        this.grid = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ];
+
+
+    }
+}
+
+
+
+function setup() {
+    // initialize all objects
+}
+
+function update() {
+    //to do: update all game objects before we render the next frame
+
+}
+
+function fraw() {
+    update();
+    //to do : render all objects frame by frame
+}
