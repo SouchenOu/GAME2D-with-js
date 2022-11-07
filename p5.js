d55c01876eea69324d0ef8c7b96c599329dbf45c
Soukaina Ouchen <souchen@e2r8p1.1337.ma>
2022-11-07 14:03:56 +0100

    add some functions

---
+++ p5.js
@@ -0,0 +1,3 @@
+/**p5.js is a JavaScript library for creative coding, with a focus on making coding accessible and inclusive for artists, designers, educators, beginners, and anyone else! p5.js is free and open-source because we believe software, and the tools to learn it, should be accessible to everyone.
+
+Using the metaphor of a sketch, p5.js has a full set of drawing functionality. However, you’re not limited to your drawing canvas. You can think of your whole browser page as your sketch, including HTML5 objects for text, input, video, webcam, and sound. */
--- raycast.js
+++ raycast.js
@@ -2,8 +2,8 @@
 const MAP_NUM_ROWS = 11;
 const MAP_NUM_COLS = 15;
 const TAILLE_SIZE = 32;
-const WINDOZ_WIDTH = MAP_NUM_COLS * TAILLE_SIZE;
-const WINDOZ_WEIGHT = MAP_NUM_ROWS * TAILLE_SIZE;
+const WINDOW_WIDTH = MAP_NUM_COLS * TAILLE_SIZE;
+const WINDOW_WEIGHT = MAP_NUM_ROWS * TAILLE_SIZE;
 class Map {
     constructor() {
         this.grid = [
@@ -21,12 +21,28 @@ class Map {
 
 
     }
+    render() {
+        for (var i = 0; i < MAP_NUM_ROWS; i++) {
+            for (var j = 0; j < MAP_NUM_COLS; j++) {
+                var tailleX = j * TAILLE_SIZE;
+                var tailleY = i * TAILLE_SIZE;
+                var tailleCOLOR = this.grid[i][j] == 1 ? "#222" : "#fff";
+                stroke("#222");
+                fill(tailleCOLOR);
+                rect(tailleX, tailleY, TAILLE_SIZE, TAILLE_SIZE);
+            }
+        }
+    }
 }
 
 
 
+var grid = new Map();
+
 function setup() {
     // initialize all objects
+    //Creates a canvas element in the document and sets its dimensions in pixels.
+    createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 function update() {
@@ -36,5 +52,6 @@ function update() {
 
 function fraw() {
     update();
+    grid.render();
     //to do : render all objects frame by frame
 }
