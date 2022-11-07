0690914b39dd85d5ee8cc94db928c22e826e56b9
Soukaina Ouchen <souchen@e2r8p1.1337.ma>
2022-11-07 16:56:11 +0100

    add the player

--- raycast.js
+++ raycast.js
@@ -13,7 +13,7 @@ class Map {
             [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
             [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
             [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
@@ -26,18 +26,44 @@ class Map {
             for (var j = 0; j < MAP_NUM_COLS; j++) {
                 var tailleX = j * TAILLE_SIZE;
                 var tailleY = i * TAILLE_SIZE;
-                var tailleCOLOR = this.grid[i][j] == 1 ? "#222" : "#fff";
+                var tailleCOLOR = this.grid[i][j] == 1 ? "blue" : "#fff";
                 stroke("#222");
                 fill(tailleCOLOR);
+                /*if (this.grid[i][j] == 1)
+                    fill("blue");
+                else if (this.grid[i][j] == 0) {
+                    fill("white");
+                }*/
                 rect(tailleX, tailleY, TAILLE_SIZE, TAILLE_SIZE);
             }
         }
     }
 }
 
+class Player {
+    constructor() {
+        this.x = WINDOW_WIDTH / 2;
+        this.y = WINDOW_HEIGHT / 2;
+        this.radius = 3;
+        this.turnDirection = 0;
+        this.walkDirection = 0;
+        this.rotationAngle = Math.PI / 2;
+        this.moveSpeed = 2.0;
+        this.rotationSpeed = 2 * (Math.PI / 180);
 
-console.log("hello");
+    }
+    update() {}
+    render() {
+        fill("black");
+        circle(this.x, this.y, 20);
+        //rect(this.x, this.y, 30, 30);
+    }
+
+}
+
 var grid = new Map();
+var player = new Player();
+
 
 function setup() {
     // initialize all objects
@@ -47,13 +73,18 @@ function setup() {
 
 function update() {
     //to do: update all game objects before we render the next frame
+    player.update();
 
 }
 
 function draw() {
+
+    player.render();
     update();
     grid.render();
+
+
+
+
     //to do : render all objects frame by frame
 }
-
-setup();
